77405f8d31fdd4aa863712e42cf746b8fd71182f
PauloDanielCarneiro <pdc187@gmail.com>
2017-11-05 00:49:46 +0000

    agora vai

--- VisOf/scrInteractor.h
+++ VisOf/scrInteractor.h
@@ -89,9 +89,9 @@ class scrInteractor
 
 			
 			
-        bool Init(float xmin, float xmax, float ymin, float ymax, float near_z, float far_z);
+        bool Init(float xmin, float xmax, float ymin, float ymax, float near_z, float far_z, int *argc, char **argv);
 		
-        bool Init (auxRect<float> rec_extents, float near_z, float far_z);
+        bool Init (auxRect<float> rec_extents, float near_z, float far_z,int *argc, char **argv);
 
 		void Display();
 
@@ -138,9 +138,17 @@ class scrInteractor
 		
         void SetScreenImageFilename(std::string filename);
         std::string& GetScreenImageFilename();
-        bool WriteScreenImage();
+		bool WriteScreenImage();
 		
-		
+		//[EP] ----VARIAVEIS QUE RETORNAM COORDENADAS DO CLIQUE DO MOUSE----
+		double getPX(); //captura x esquerdo
+		double getPY(); //captura y esquerdo
+		double getPXD(); //captura x direito
+		double getPYD(); //captura y direito
+		bool getMouseLeft();  //captura se clique esquerdo
+		bool getMouseRight(); //captura se clique direito
+		//[FIM]
+
 	protected :
 	
 		/*Criar uma variavel centroide para armazenar o centroide =)*/
@@ -163,7 +171,7 @@ class scrInteractor
 		void Draw_String(char * text);
 
 		void Draw_Axis();
-
+		
 		void Init_List();
 	
 		void Calc_Scale();
@@ -171,6 +179,8 @@ class scrInteractor
 		void Calc_Translation();
 
 		void Initial_Translation ();
+        
+        void Initial_Rotation();
 
 		void Init_Lights();
 
