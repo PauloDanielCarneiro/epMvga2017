212d5f561ac47fb00e828e05f99fa10fb43f9c6e
PauloDanielCarneiro <pdc187@gmail.com>
2017-11-05 01:42:34 +0000

    corrigindo caminho fileBRasil

--- main.cpp
+++ main.cpp
@@ -122,7 +122,7 @@ int main(int *argc, char **argv)
   ofVtkWriter<MyofDefault2D> writer;
   Interactor->setDraw(RenderScene);
 	meshHandler.Set(new TMesh());
-      char *fileBrasil = "/home/helton/Downloads/ep_mvga/epMvga2017/Brasil.off";
+      char *fileBrasil = "/home/paulo/Downloads/epMvga2017/Brasil.off";
 
      
     reader.readOffFile(fileBrasil);
